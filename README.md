47528bfbaaa427bd5140cddc909ef030f97caa62
Tomas Morris <tomas.morris13@gmail.com>
2012-10-17 10:45:35 -0600

    Working recursive functionality, need dynamic

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ algorithmsProject1
 ==================
 
 Project 1 for CSCI 406
+
---
+++ palindromer.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+from sys import argv
+
+def palindrominate(inString):
+	if(len(inString) == 0):
+		return ""
+
+	longest = ""
+	for i in range(0,len(inString)-1):
+		curLetter = inString[i]
+		farthestIndex = inString.rindex(curLetter);
+		pal = ""		
+		if(farthestIndex == i):
+			pal = curLetter
+		else : 
+			pal = curLetter + palindrominate(inString[i+1:farthestIndex]) + curLetter
+		
+		if(len(pal) > len(longest)):
+			longest = pal
+
+	return longest
+
+print palindrominate(argv[1])
